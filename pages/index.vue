f7cf690a7bd0ab490ccc48d5190435c2f0d71399
Yosua Tarida Panjaitan <lomylix@gmail.com>
2024-11-30 16:48:13 +0700

    add card

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,24 @@
 <template>
   <UContainer class="flex h-dvh">
-    <div class="m-auto w-full max-w-sm">
-      <UForm :state="state" class="space-y-4" @submit="onSubmit">
+    <UCard class="m-auto w-full max-w-sm">
+      <template #header>
         <h1 class="text-xl font-bold">Login</h1>
+      </template>
+
+      <UForm :state="state" class="space-y-4" @submit="onSubmit">
         <UFormField label="Email" name="email">
           <UInput class="w-full" v-model.trim="state.email" />
         </UFormField>
 
         <UButton type="submit" label="Log in" block />
       </UForm>
-    </div>
+
+      <template #footer>
+        <p class="text-sm text-[var(--ui-text-muted)]">
+          Don't have an account? <span class="text-[var(--ui-primary)] font-semibold">Sign up</span>
+        </p>
+      </template>
+    </UCard>
   </UContainer>
 </template>
 
